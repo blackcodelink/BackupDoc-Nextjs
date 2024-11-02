6bb8527ea287da6723c28340c82af3d6400b072f
Black Codelink <blackcodelink@gmail.com>
2024-11-02 17:16:44 +0530

    HomeAbout

---
+++ src/config.js
@@ -0,0 +1,71 @@
+import dailyNews from './images/logos/daily-news.svg'
+import netflix from './images/logos/netflix.svg'
+import github from './images/logos/github.svg'
+import tinder from './images/logos/tinder.svg'
+import apple from './images/logos/apple.svg'
+import ticketmaster from './images/logos/ticketmaster.svg'
+import amazon from './images/logos/amazon.svg'
+import stripe from './images/logos/stripe.svg'
+import facebook from './images/logos/facebook.svg'
+import oracle from './images/logos/oracle.svg'
+import mavinDental from '@/images/logos/mavinDental.svg'
+
+import smileSolution from './images/logos/smileSolution.png'
+
+import twitterIcon from '@/icons/nucleo/twitter.svg'
+import githubIcon from '@/icons/nucleo/github.svg'
+import youtubeIcon from '@/icons/nucleo/youtube.svg'
+import discordIcon from '@/icons/nucleo/discord.svg'
+import instagramIcon from '@/icons/nucleo/instagram.svg'
+
+export const SOCIALS = [
+  {
+    name: 'twitter',
+    href: '#',
+    label: 'See what’s new',
+    ariaLabel: 'Follow on Twitter',
+    icon: twitterIcon,
+  },
+  {
+    name: 'github',
+    href: '#',
+    label: 'See how it’s done',
+    ariaLabel: 'Follow on Github',
+    icon: githubIcon,
+  },
+  {
+    name: 'youtube',
+    href: '#',
+    label: 'Watch tutorials',
+    ariaLabel: 'Follow on Youtube',
+    icon: youtubeIcon,
+  },
+  {
+    name: 'discord',
+    href: '#',
+    label: 'Talk to us',
+    ariaLabel: 'Join our Discord',
+    icon: discordIcon,
+  },
+  {
+    name: 'instagram',
+    href: '#',
+    label: 'Life at Nebula',
+    ariaLabel: 'Follow on Instagram',
+    icon: instagramIcon,
+  },
+]
+
+export const CLIENTS = [
+  { name: 'OroDental', logo: '/images/11.svg' },
+  { name: 'mavinDental', logo: '/images/2.svg' },
+  { name: 'bhupaDental', logo: '/images/3.svg' },
+
+  { name: 'smileSolution', logo: '/images/4.svg' },
+  { name: 'Lumino', logo: '/images/5.svg' },
+  { name: 'Dental Artisary', logo: '/images/6.svg' },
+  { name: 'MaxcareDental', logo: '/images/7.svg' },
+  { name: 'Luna', logo: '/images/8.svg' },
+  { name: 'ZenithDental', logo: '/images/9.svg' },
+  { name: 'WhiteDental', logo: '/images/10.svg' },
+]
